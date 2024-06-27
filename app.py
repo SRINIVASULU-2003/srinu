cb9c71548bf735c1a1edee70e13cdfc6b24b76a6
SRINIVASULU-2003 <srinivasulu@redfernstech.com>
2024-06-27 15:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from gradio_client import Client
 app = Flask(__name__)
 
 # Initialize Gradio client
-client = Client("srinuksv/SRUNU")
+client = Client("srinukethanaboina/ME")
 
 # Define endpoint for webhook
 @app.route('/webhook', methods=['POST'])
