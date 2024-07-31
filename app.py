630c3591bc67aa63b64e3eaa642703e0c528b184
SRINIVASULU-2003 <srinivasulu@redfernstech.com>
2024-07-31 19:07:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('form.html')
-@app.route('//home')
+@app.route('/home')
 def index():
     return render_template('index.html')
 @app.route('/chatbot')
