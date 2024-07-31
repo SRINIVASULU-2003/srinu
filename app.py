f075616121371226ed70248fe538f10d63ef5163
SRINIVASULU-2003 <srinivasulu@redfernstech.com>
2024-07-31 19:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index1():
     return render_template('form.html')
 @app.route('/home')
 def index():
